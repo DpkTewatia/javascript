83b57c7fe249d82105c3a1f1a5baae2a747c3a5d
Dpk Tewatia <deepaktewatia01@gmail.com>
2021-12-07 23:57:07 +0530

    done

--- app.js
+++ app.js
@@ -13,50 +13,39 @@
 //array and loops----------------------------------------------------------------------------------------------------------------
 
 
-// const actors = [ 
-//     {     
-//     name: 'actor 1',
-//     payment: 100
-//     },
-//     {     
-//     name: 'actor 2',
-//     payment: 100
-//     } ,
-//     {     
-//     name: 'actor 3', 
-//     payment: 100
-//     }
-// ];
+const actors = [ 
+    {     
+    name: 'actor 1',
+    payment: 100
+    },
+    {     
+    name: 'actor 2',
+    payment: 150
+    } ,
+    {     
+    name: 'actor 3', 
+    payment: 200
+    }
+];
 
-// for(let i=0; i<actors.length; i++){
-//     console.log(actors[i])
+for(let i=0; i<actors.length; i++){
+    console.log(actors[i])
 
-//     console.log(actors.length); 
-// }
-// console.log(actors.length); 
+    console.log(actors.length); 
+}
+console.log(actors.length); 
 
 //----------------------------------------------------------------------------------------------
-
-const actors = [ 
-        {     
-        name: 'actor 1',
-        payment: 100
-        },
-        {     
-        name: 'actor 2',
-        payment: 150
-        } ,
-        {     
-        name: 'actor 3', 
-        payment: 200
-        }
-    ];
+    //function to deduct 10 rs fromo all actors
     
     for(let i=0; i<actors.length; i++){
         actors[i].payment=actors[i].payment -10;   //overacting k 10 rs kaat liye
     }    
     console.log(actors);
 //----------------------------------------------------------------------------------------------
+    //for each loop
+
+
 
 
 
